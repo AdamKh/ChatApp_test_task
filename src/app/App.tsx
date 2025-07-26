6c4526f9dbd1db00ffbe42698e47e6b0574c81f9
AdamKh <adamkhashiev2004@mail.ru>
2025-07-26 16:23:29 +0300

    feat: added aliases

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import styles from './styles/App.module.scss'
-import Home from '../pages/home'
-import Layout from '../shared/ui/Layout/Layout'
+import Home from '@pages/home'
+import Layout from '@shared/ui/Layout/Layout'
 
 const App: React.FC = () => {
   return (
     <main className={styles.main}>
       <Layout>
         <Home />
-        {/* Здесь можно добавить другие компоненты */}
       </Layout>
     </main>
   )
